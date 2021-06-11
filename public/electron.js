ab3446e423c742fbc104ef5cf1edf62033e365d9
Stijn Piron <stijn.piron@gmail.com>
2021-06-11 12:01:57 +0200

    quit thinking about db, local file storage is the way to go!

--- public/electron.js
+++ public/electron.js
@@ -2,6 +2,19 @@ const electronUpdater = require('electron-updater');
 const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
 const path = require('path');
+const Store = require('electron-store');
+
+// First instantiate the class
+const store = new Store({
+  // We'll call our data file 'user-preferences'
+  name: 'user-preferences',
+  defaults: {
+    // 800x600 is the default size of our window
+    windowBounds: { width: 800, height: 600 },
+  },
+  // encryptionKey: 'encryption key', // use node-keytar to store the encryption key safely
+  // fileExtension: '',
+});
 
 // Conditionally include the dev tools installer to load React Dev Tools
 let installExtension, REACT_DEVELOPER_TOOLS;
@@ -18,16 +31,26 @@ if (isDev) {
   REACT_DEVELOPER_TOOLS = devTools.REACT_DEVELOPER_TOOLS;
 }
 
-function createWindow() {
+function createWindow(width, height) {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width,
+    height,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
       enableRemoteModule: false,
       preload: path.join(__dirname, 'preload.js'),
     },
+  });
+
+  // The BrowserWindow class extends the node.js core EventEmitter class, so we use that API
+  // to listen to events on the BrowserWindow. The resize event is emitted when the window size changes.
+  mainWindow.on('resize', () => {
+    // The event doesn't pass us the window size, so we call the `getBounds` method which returns an object with
+    // the height, width, and x and y coordinates.
+    let { width, height } = mainWindow.getBounds();
+    // Now that we have them, save them using the `set` method.
+    store.set('windowBounds', { width, height });
   });
 
   // Load from localhost if in development
@@ -50,7 +73,9 @@ app.whenReady().then(() => {
       .catch((error) => console.log(`An error occurred: , ${error}`));
   }
 
-  createWindow();
+  let { width, height } = store.get('windowBounds');
+
+  createWindow(width, height);
 });
 
 // Add a new listener that tries to quit the application when
